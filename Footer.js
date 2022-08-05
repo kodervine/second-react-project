02de330d88a2cad6ff6b630fb73bef2385a2f48f
Laskellyb <anikwenzekelly@gmail.com>
2022-08-05 08:24:28 +0100

    creating different files

---
+++ Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function Footer() {
+    return (
+        <footer className = "footer">
+            <small>© 2022 Anikwenze development. All rights reserved.</small>
+        </footer>
+    )
+}
--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Header from "./Header"
-
-function Main() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>I can build projects faster</li>
-                <li>I will be more employable</li>
-                <li>I will improve my programming skills</li>
-            </ol>
-        </div>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className = "footer">
-            <small>© 2022 Anikwenze development. All rights reserved.</small>
-        </footer>
-    )
-}
+import Main from "./Main"
+import Footer from "./Footer"
 
 function Page() {
     return (
