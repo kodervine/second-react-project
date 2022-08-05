8a2bdb22236d5def135361a3e4f20c67cdb50730
Laskellyb <anikwenzekelly@gmail.com>
2022-08-05 07:59:39 +0100

    Styling

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-/**
-Challenge: 
-
-- Add an `ul` inside the Header's `nav` and create
-  the following `li`s: "Pricing", "About", & "Contact"
-*/
-
-function Header() {
-    return (
-        <header>
-            <nav className = "navbar">
-                <img src="./react-logo.png" className="nav-logo" />
-                <ul className = "list-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function Footer() {
-    return (
-        <footer className = "footer">
-            <small>© 2022 Anikwenze development. All rights reserved.</small>
-        </footer>
-    )
-}
+import Header from "./Header"
 
 function Main() {
     return (
@@ -41,6 +12,14 @@ function Main() {
                 <li>I will improve my programming skills</li>
             </ol>
         </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className = "footer">
+            <small>© 2022 Anikwenze development. All rights reserved.</small>
+        </footer>
     )
 }
 
